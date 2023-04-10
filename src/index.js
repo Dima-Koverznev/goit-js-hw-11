199ec8c7e6b1defa1d1001d4960b5c4ace01f6f0
Dima-Koverznev <dimakoverznev99@gmail.com>
2023-04-10 12:06:40 +0300

    finish work

---
+++ src/index.js
@@ -0,0 +1,119 @@
+import { uhu } from 'uhugrid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import imageItemHbs from './templates/image-item.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import refsEl from './js/refs.js';
+import { Images } from './js/fatchImages.js';
+import throttle from 'lodash.throttle';
+
+const refs = refsEl();
+
+refs.form.addEventListener('submit', handleSubmitForm);
+refs.imgList.addEventListener('click', handleClickImage);
+refs.sizeRange.addEventListener('input', throttle(handleChangeRange, 300));
+refs.heightRange.addEventListener('input', throttle(handleChangeRange, 300));
+window.addEventListener('scroll', throttle(checkPosition, 300));
+
+const images = new Images();
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+Notify.init({
+  width: '300px',
+  position: 'right-top',
+  closeButton: false,
+  timeout: 3000,
+  cssAnimation: true,
+  cssAnimationDuration: 400,
+  cssAnimationStyle: 'fade',
+});
+
+function handleChangeRange(e) {
+  updateGallery();
+}
+
+function updateGallery() {
+  const size = refs.sizeRange.value;
+  const height = refs.heightRange.value;
+  uhu(size, height);
+}
+
+function handleClickImage(e) {
+  if (e.target.nodeName === 'IMG') {
+    lightbox.on('show.simplelightbox');
+  }
+}
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  clearHTML();
+  images.setFistPage();
+  const query = e.target.elements.query.value.trim();
+
+  if (!query) {
+    return;
+  }
+
+  images.setValue(query);
+  fatchImages();
+  e.target.elements.query.value = '';
+}
+
+function fatchImages() {
+  images
+    .fatchImages()
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      renderHTML(data.hits);
+
+      if (images.getPageNumber() === 1) {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        updateGallery();
+      }
+
+      lightbox.refresh();
+      images.incrementPage();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function renderHTML(data) {
+  refs.imgList.insertAdjacentHTML('beforeend', imageItemHbs(data));
+}
+
+function checkPosition() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+
+  const threshold = height - screenHeight / 4;
+  const position = scrolled + screenHeight;
+
+  if (position >= threshold) {
+    fatchImages();
+    smoothScroll();
+  }
+}
+
+function clearHTML() {
+  refs.imgList.innerHTML = '';
+}
+
+const smoothScroll = () => {
+  const { height: cardHeight } = document
+    .querySelector('.gallery-all')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+};
---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export default function refsEl() {
+    return {
+      form: document.querySelector('.search-form'),
+      imgList: document.querySelector('.gallery-all'),
+      sizeRange: document.querySelector('[data-volume="size"]'),
+      heightRange: document.querySelector('[data-volume="height"]'),
+    };
+  }
